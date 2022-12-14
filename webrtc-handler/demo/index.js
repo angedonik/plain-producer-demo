72125edea3f38540a343f5ecc2b57bce20da2c31
Alexander Kiyashko <angedonik@gmail.com>
2022-12-14 13:40:42 +0200

    update

--- webrtc-handler/demo/index.js
+++ webrtc-handler/demo/index.js
@@ -1,4 +1,4 @@
-const {Utils,DemoClient}=window.DemoClientLib;
+const {DemoClient}=window.DemoClientLib;
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
